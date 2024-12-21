0969b9f6e787ff19d2c1794211ca153afe5ae7e7
Adrien Valo <adrien.valo@outlook.fr>
2024-12-21 22:22:02 +0100

    stabilisation du projet Angular

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { TaskComponent } from './components/task/task.component';
 
-export const routes: Routes = [];
+export const appRoutes: Routes = [
+  { path: 'home', component: HomeComponent },
+  { path: 'tasks', component: TaskComponent },
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+];
