a54e272926714c7685cc990de49255c5094cd5f9
Adrien Valo <adrien.valo@outlook.fr>
2025-01-08 11:03:49 +0100

    ajout des routes dans app.route.ts

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,21 +2,25 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
-import { appRoutes } from './app.routes';  // Import de tes routes
+import { appRoutes } from './app.routes';  // Import des routes
 import { provideHttpClient } from '@angular/common/http';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { TaskComponent } from './components/task/task.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [
+    AppComponent,
+    HomeComponent, // Déclarer les composants ici
+    TaskComponent, // Déclarer les composants ici
+  ],
   imports: [
     CommonModule,
-    RouterModule.forRoot(appRoutes), 
-    
+    RouterModule.forRoot(appRoutes),
   ],
   providers: [
-    provideHttpClient()  // Fournit HttpClient sans HttpClientModule
+    provideHttpClient()  // Fournit HttpClient
   ],
-  exports: [RouterModule],
-  bootstrap: [AppComponent]  
+  bootstrap: [AppComponent],
 })
 export class AppConfig {}
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,17 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
 import { TaskComponent } from './components/task/task.component';
+import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
+import { ProjectsComponent } from './components/projects/projects.component';
+import { ProjectDetailComponent } from './components/project-detail/project-detail.component';
 
 export const appRoutes: Routes = [
-  { path: 'home', component: HomeComponent },
-  { path: 'tasks', component: TaskComponent },
-  { path: '', redirectTo: '/home', pathMatch: 'full' } // route par défaut
+  { path: 'login', component: LoginComponent },
+  { path: 'register', component: RegisterComponent },
+  { path: 'projects', component: ProjectsComponent },
+  { path: 'projects/:id', component: ProjectDetailComponent },
+  { path: 'tasks/:id', component: TaskComponent },
+  { path: '', redirectTo: '/login', pathMatch: 'full' }, // Redirection par défaut vers la page de login
+  { path: '**', redirectTo: '/login', pathMatch: 'full' } // Gestion des routes inconnues
 ];
