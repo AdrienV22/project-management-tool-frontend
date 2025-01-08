2217e51edb0d59f8c8cc6939ef61fa5f6f70e657
Adrien Valo <adrien.valo@outlook.fr>
2025-01-08 16:14:46 +0100

    Modification du fichier app.component.ts : bug corrigé

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 @Component({
+  standalone: true, // Indique que c'est un standalone component
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css'] 
+  styleUrls: ['./app.component.css'],
+  imports: [RouterModule], // Ajoute RouterModule ici
 })
 export class AppComponent {
   title = 'Mon application de project management';
