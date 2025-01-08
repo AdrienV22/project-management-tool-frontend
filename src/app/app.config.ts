761885a91c0655a7ba22b595f1ace8b836601e01
Adrien Valo <adrien.valo@outlook.fr>
2025-01-08 18:04:13 +0100

    modification du app.component.html avec tous les nouveaux liens de navigation

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,12 +1,25 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { BrowserModule } from '@angular/platform-browser'; // Ajoute BrowserModule ici
+import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule } from '@angular/router';
 import { appRoutes } from './app.routes';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
+import { ProjectsComponent } from './components/projects/projects.component';
+import { TaskComponent } from './components/task/task.component';
+import { HomeComponent } from './components/home/home.component';
 import { provideHttpClient } from '@angular/common/http';
 
 @NgModule({
+  declarations: [
+    AppComponent,
+    LoginComponent,
+    RegisterComponent,
+    ProjectsComponent,
+    TaskComponent,
+    HomeComponent // Déclarer ici ton HomeComponent
+  ],
   imports: [
     BrowserModule,  // Utilise BrowserModule pour les applications dans le navigateur
     CommonModule,
