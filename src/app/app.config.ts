cc06fd62476bc66d46e3f9598d5aed5c0ecdd15c
Adrien Valo <adrien.valo@outlook.fr>
2026-02-16 22:50:49 +0100

    feat(security): implement stateless JWT authentication and route protection

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,38 +1,14 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { BrowserModule } from '@angular/platform-browser';
-import { RouterModule } from '@angular/router';
+import { ApplicationConfig } from '@angular/core';
+import { provideRouter } from '@angular/router';
 import { appRoutes } from './app.routes';
-import { provideHttpClient } from '@angular/common/http';
-import { FormsModule } from '@angular/forms';
-import { DragDropModule } from '@angular/cdk/drag-drop';
 
-// Importer les composants standalone
-import { AppComponent } from './app.component';
-import { LoginComponent } from './components/login/login.component';
-import { RegisterComponent } from './components/register/register.component';
-import { ProjectsComponent } from './components/projects/projects.component';
-import { TaskComponent } from './components/task/task.component';
-import { HomeComponent } from './components/home/home.component';
-import { TaskListComponent } from './components/task-list/task-list.component'
+import { provideHttpClient, withInterceptorsFromDi, HTTP_INTERCEPTORS } from '@angular/common/http';
+import { JwtInterceptor } from './interceptors/jwt.interceptor';
 
-@NgModule({
-  imports: [
-    BrowserModule,
-    CommonModule,  // Importation de CommonModule pour *ngIf, *ngFor
-    RouterModule.forRoot(appRoutes),  // Configuration des routes
-    FormsModule,  // Importation de FormsModule pour les formulaires
-    DragDropModule, // Module pour le drag & drop
-    LoginComponent,
-    RegisterComponent,
-    ProjectsComponent,
-    TaskComponent,
-    HomeComponent,
-    TaskListComponent,  // Ajout de TaskListComponent
-  ],
+export const appConfig: ApplicationConfig = {
   providers: [
-    provideHttpClient(), // Fournit HttpClient
+    provideRouter(appRoutes),
+    provideHttpClient(withInterceptorsFromDi()),
+    { provide: HTTP_INTERCEPTORS, useClass: JwtInterceptor, multi: true },
   ],
-  bootstrap: [AppComponent],  // Bootstrap de l'application avec AppComponent
-})
-export class AppConfig {}
+};
